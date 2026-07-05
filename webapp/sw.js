2ffda7371469f801713df095804af4603adb9efb
JonathassBarbosa <jonathasbarbosa@live.com>
2026-07-05 12:34:31 -0300

    Add files via upload

---
+++ webapp/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'dimdim-v3';
+const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/mark-transparent.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.hostname.includes('script.google.com')) return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => cached))
+  );
+});
